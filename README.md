65f593d3e063cb304ece3ebeefdf01ce09706301
Charles ORiley <ceoriley@gmail.com>
2024-06-24 16:28:27 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+Coding Exercise: Decoding a Message from a Text File
+In this exercise, you will develop a function named decode(message_file). The message_file argument is a string containing the file path for a .txt file that has an encoded message. When the function is called, it should read the file specified by its argument, decode the message, and return (not print) the decoded message as a string in a specific format, described below.
+
+Note that you can write your code using any language and IDE you want (Python is preferred if possible, but not mandatory).
+
+Your function must be able to process an input file with the following format:
+
+3 love
+6 computers
+2 dogs
+4 cats
+1 i
+5 you
+
+In this file, each line contains a number followed by a word. The task is to decode a hidden message based on the arrangement of these numbers into a "pyramid" structure. The numbers are placed into the pyramid in ascending order, with each line of the pyramid having one more number than the line above it. The smallest number is 1, and the numbers increase consecutively, like so:
+
+  1
+ 2 3
+4 5 6
+
+The key to decoding the message is to use the words corresponding to the numbers at the end of each pyramid line (in this example, 1, 3, and 6). The decoded message is formed by taking these words and separating them by individual spaces, with no extra characters. You should ignore all the other words. So for the example input file above, the message words are:
+
+1: i
+3: love
+6: computers
+and your function would return the string "i love computers".
---
+++ src/main.rs
@@ -0,0 +1,87 @@
+use std::collections::HashMap;
+use std::fs;
+use std::io::BufRead;
+use std::path::{Path, PathBuf};
+use walkdir::WalkDir;
+
+fn find_file(name: &str, start_dir: &Path) -> Option<PathBuf> {
+    for entry in WalkDir::new(start_dir).into_iter().filter_map(Result::ok) {
+        let path = entry.path();
+        if path.file_name().map_or(false, |f| f == name) {
+            return path.canonicalize().ok();
+        }
+    }
+    None
+}
+
+fn decode(message_file: &str) -> String {
+    let mut key_value_map: HashMap<u32, String> = HashMap::new();
+
+    if let Ok(lines) = read_lines(&message_file) {
+        for line in lines {
+            if let Ok(ip) = line {
+                let parts: Vec<&str> = ip.split_whitespace().collect();
+                if parts.len() == 2 {
+                    if let Ok(key) = parts[0].parse::<u32>() {
+                        key_value_map.insert(key, parts[1].to_string());
+                    }
+                }
+            }
+        }
+    }
+
+    let mut decoded_message = String::new();
+    /*
+    move the message away from the prompt.
+    */
+    decoded_message.push_str("\n");
+    let mut current_key: u32 = 1;
+    /*
+    this will be the iterative counter.
+    */
+    let mut i: u32 = 1;
+
+    loop {
+        i += 1;
+        match key_value_map.get(&current_key) {
+            Some(value) => {
+                decoded_message.push_str(value);
+                decoded_message.push_str(" ");
+                current_key = current_key + i;
+            }
+            None => break,
+        }
+    }
+    decoded_message
+}
+
+fn read_lines(
+    file_path: &str,
+) -> Result<impl Iterator<Item = Result<String, std::io::Error>>, std::io::Error> {
+    let file = fs::File::open(file_path)?;
+    Ok(std::io::BufReader::new(file).lines())
+}
+
+fn main() {
+    let file_name = "coding_tree.txt";
+    /*
+    Starting directory for search.
+    The runtime for this process
+    will depend on the preciseness
+    of the path entered.
+    */
+    let start_directory = std::path::Path::new("/Users"); 
+
+    match find_file(file_name, start_directory) {
+        Some(file_path) => {
+            let message_file = file_path.display().to_string();
+            let decoded_message = decode(&message_file);
+            /*
+            proof of concept.
+            */
+            println!("{decoded_message}");
+
+        }
+        None => eprintln!("File not found"),
+    }
+}
